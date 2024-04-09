1d13225f1d48bfccaf8753c7eb83e4205f406390
Vladimir Frolov <frolov_vo@inbox.ru>
2024-04-09 19:50:46 +0300

    feat: create Events Tab

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -2,13 +2,18 @@ import './Profile.css';
 import Breadcrumbs from '../../components/Breadcrumbs/Breadcrumbs';
 import UserBar from './UserBar/UserBar';
 import { userData } from '../../utils/mock-data';
+import EventsTab from '../../components/EventsTab/EventsTab';
+import { cardsData } from '../../utils/mock-data';
 
 function Profile() {
+  const expectedEvents = cardsData.filter((event) => Date.now() - event.date.getTime() < 0);
+  const completedEvents = cardsData.filter((event) => Date.now() - event.date.getTime() > 0);
   return (
     <main className="profile">
       <div className="profile__container">
         <Breadcrumbs />
         <UserBar user={userData} />
+        <EventsTab expectedEvents={expectedEvents} completedEvents={completedEvents} />
       </div>
     </main>
   );
