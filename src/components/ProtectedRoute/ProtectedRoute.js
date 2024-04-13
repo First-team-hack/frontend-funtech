90860b9f7024077345759d4f7c8c6256993cfd99
Vladimir Frolov <frolov_vo@inbox.ru>
2024-04-14 01:20:36 +0300

    fix: protected route now replace current entry in history stack whken redirect

--- src/components/ProtectedRoute/ProtectedRoute.js
+++ src/components/ProtectedRoute/ProtectedRoute.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Navigate } from 'react-router-dom';
 
 function ProtectedRoute({ component: Component, ...props }) {
-  return props.isLoggedIn ? <Component {...props} /> : <Navigate to="/auth" />;
+  return props.isLoggedIn ? <Component {...props} /> : <Navigate to="/auth" replace={true} />;
 }
 
 export default ProtectedRoute;
